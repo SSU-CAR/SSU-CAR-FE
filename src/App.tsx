a8491709d8962e8895ebbd2fc06c5d87974ad004
jwo0o0 <jwo0o0358@gmail.com>
2024-02-10 20:10:04 +0900

    feat: set react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { GlobalStyle } from "@styles/globalStyle";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { HomePage } from "@pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <BrowserRouter>
+      <GlobalStyle />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
