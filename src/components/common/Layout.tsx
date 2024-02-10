53feea492fdcc1b8849350d47c107a051137f87e
김정우 <jwo0o0358@gmail.com>
2024-02-10 22:03:49 +0900

    Test Deploy (#9)
    * Create PULL_REQUEST_TEMPLATE.md
    
    * Update PULL_REQUEST_TEMPLATE.md
    
    * Update PULL_REQUEST_TEMPLATE.md
    
    * [Feat] 개발 환경 세팅 (#2)
    
    * feat: 필요없는 파일 정리
    
    * chore: 패키지 설치
    
    * style: set Global Style
    
    * feat: set absolute path
    
    * feat: set react router
    
    * chore: 웹폰트 파일 추가(Pretendard, 현대하모니)
    
    * style: 웹 폰트 font-family 설정
    
    * style: theme, colors 설정
    
    * feat: Layout 컴포넌트, 하단바 조건부 렌더링 설정
    
    * Create aws.yml

---
+++ src/components/common/Layout.tsx
@@ -0,0 +1,49 @@
+import styled from "styled-components";
+import { ReactNode, useEffect } from "react";
+import { Navbar } from "./Navbar";
+import { theme } from "@styles/theme";
+
+interface LayoutProps {
+  children: ReactNode;
+  navbar?: boolean;
+}
+
+export const Layout = ({ children, navbar }: LayoutProps) => {
+  /**
+   * mobile height size 설정
+   */
+  useEffect(() => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty("--vh", `${vh}px`);
+
+    window.addEventListener("resize", () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    });
+  }, []);
+
+  return (
+    <LayoutContainer>
+      {children}
+      {navbar && <Navbar />}
+    </LayoutContainer>
+  );
+};
+
+const LayoutContainer = styled.div`
+  width: 100%;
+  max-width: 460px;
+  height: 100vh;
+  height: calc(var(--vh, 1vh) * 100);
+  margin: 0px auto;
+  background-color: ${theme.colors.gray0};
+
+  overflow-y: scroll;
+
+  &::-webkit-scrollbar {
+    /* ( 크롬, 사파리, 오페라, 엣지 ) 동작 */
+    display: none;
+  }
+  -ms-overflow-style: none; /* 인터넷 익스플로러 */
+  scrollbar-width: none; /* 파이어폭스 */
+`;
