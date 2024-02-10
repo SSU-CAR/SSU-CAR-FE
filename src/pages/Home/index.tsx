308dd2a12ec8108658a5658b4cb9b029840249a2
jwo0o0 <jwo0o0358@gmail.com>
2024-02-10 21:05:10 +0900

    feat: Layout 컴포넌트, 하단바 조건부 렌더링 설정

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,14 @@
+import styled from "styled-components";
+import { Layout } from "@components/common/Layout";
+
 export const HomePage = () => {
   return (
-    <>
-      <div>home</div>
-    </>
+    <Layout navbar={true}>
+      <Title>안녕하세요</Title>
+    </Layout>
   );
 };
+
+const Title = styled.div`
+  font-family: "Pretendard";
+`;
