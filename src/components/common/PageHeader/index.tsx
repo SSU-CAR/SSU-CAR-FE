aadfb07797fc8722aa19c373709ebe67e7af8bcb
jwo0o0 <jwo0o0358@gmail.com>
2024-02-15 21:09:18 +0900

    fix: 공통 페이지 헤더 height 수정

--- src/components/common/PageHeader/index.tsx
+++ src/components/common/PageHeader/index.tsx
@@ -6,18 +6,28 @@ interface PageHeaderProps {
 }
 
 export const PageHeader = ({ name, height }: PageHeaderProps) => {
-  return <PageHeaderContainer height={height}>{name}</PageHeaderContainer>;
+  return (
+    <PageHeaderContainer height={height}>
+      <PageNameText>{name}</PageNameText>
+    </PageHeaderContainer>
+  );
 };
 
 const PageHeaderContainer = styled.div<{ height?: string }>`
   width: 100%;
   height: ${(props) => props.height || "60px"};
-  padding: 0 30px;
 
   background-color: white;
   font-family: "Pretendard";
   font-weight: 700;
   font-size: 20px;
   color: ${({ theme }) => theme.colors.blue800};
-  line-height: ${(props) => props.height || "60px"};
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const PageNameText = styled.div`
+  width: 360px;
 `;
