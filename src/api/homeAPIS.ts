47f569cd8dacf35237a712e40dd02f2553239bbf
jwo0o0 <jwo0o0358@gmail.com>
2024-02-21 18:46:15 +0900

    홈 화면 API 추가

--- src/api/homeAPIS.ts
+++ src/api/homeAPIS.ts
@@ -10,10 +10,29 @@ export const homeScoreAPI = async () => {
   }
 };
 
+export const homeLatestScoreAPI = async () => {
+  try {
+    const response = await axiosInstance.get("/home/latestScores");
+    return response.data;
+  } catch {
+    return null;
+  }
+};
+
 //최근 주행 피드백
 export const homeFeedbackAPI = async () => {
   try {
     const response = await axiosInstance.get("/home/feedback");
+    return response.data;
+  } catch {
+    return null;
+  }
+};
+
+//주의해야 할 운전 습관
+export const homeRecentsAPI = async () => {
+  try {
+    const response = await axiosInstance.get("/home/recentRisks");
     return response.data;
   } catch {
     return null;
