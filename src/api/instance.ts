35abbc79f3dab40fda90e983ec8d6e753f3ce2a4
jwo0o0 <jwo0o0358@gmail.com>
2024-02-22 16:52:33 +0900

    fix: 서버 IP 변경

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://3.38.211.169:8080",
+  baseURL: "http://15.164.253.57:8080",
   headers: {
     "Content-Type": "application/json",
   },
